71a69bc749f97f3c9219880715ffa076d0699764
FenteMontoto <fentemontoto@gmail.com>
2022-12-30 13:25:14 +0100

    Traductor de fullcalendar

--- albergue/applications/reservas/admin.py
+++ albergue/applications/reservas/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 from .models import Reserva, User
-
+from .forms import ReservaRegisterForm
 
 class ReservaAdmin(admin.ModelAdmin):
+     form:ReservaRegisterForm
      list_display=('nombre_usuario','camas_reservadas','fecha_reserva_entrada','pago_confirmado',
             'documentos',)
 
