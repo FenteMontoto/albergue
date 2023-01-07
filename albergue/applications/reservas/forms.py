39c7b7299c38a6e70a4a28a571f7bd43e6bde8f9
FenteMontoto <fentemontoto@gmail.com>
2023-01-07 12:35:59 +0100

    comprobación de camas a reservar, comprueba que no se reserven más plazas de las disponibles

--- albergue/applications/reservas/forms.py
+++ albergue/applications/reservas/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Reserva
+from django.http import JsonResponse
+from django.db.models import Sum,Count
+from django.utils.translation import gettext_lazy as _
 
 class ReservaRegisterForm(forms.ModelForm):
     
@@ -22,3 +25,23 @@ class ReservaRegisterForm(forms.ModelForm):
                 
             )
         }
+    def clean(self):
+        cleaned_data=super(ReservaRegisterForm,self).clean()
+        camas_reservadas=self.cleaned_data['camas_reservadas']
+        fecha_reserva_entrada=self.cleaned_data['fecha_reserva_entrada']
+        # consulta=Reserva.objects.raw('SELECT num_reserva,fecha_reserva_entrada, SUM(camas_reservadas) as camas_reservadas  from reserva  where fecha_reserva_entrada="fecha_reserva_entrada" group by fecha_reserva_entrada')
+        consulta=Reserva.objects.all().filter(fecha_reserva_entrada=self.cleaned_data['fecha_reserva_entrada']).aggregate(Sum('camas_reservadas'))
+        for con in consulta:
+           if consulta[con]!=None:
+                camas_disponibles=7-consulta[con]
+                if camas_disponibles==0:
+                    error=_("No hay camas disponibles para el día ")+str(fecha_reserva_entrada)
+                    raise forms.ValidationError(error)
+                if consulta[con]+camas_reservadas>7:
+                    error=_("El número de camas disponibles para el día ")+str(fecha_reserva_entrada)+ " es de " +str(camas_disponibles)
+                    raise forms.ValidationError(error)
+                
+        # self.add_error('camas_reservadas',error)
+        print (camas_reservadas)
+        print (fecha_reserva_entrada)
+            
